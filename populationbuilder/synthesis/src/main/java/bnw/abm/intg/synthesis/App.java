457c3c8771e6b8f76b6dc2a7c9e0c461b24ce657
wniroshan <s3467727@student.rmit.edu.au>
2017-12-14 20:32:14 +1100

    adding updated files

---
+++ populationbuilder/synthesis/src/main/java/bnw/abm/intg/synthesis/App.java
@@ -0,0 +1,144 @@
+package bnw.abm.intg.synthesis;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import bnw.abm.intg.filemanager.csv.abs.ABSStatisticalAreaCodeConverter;
+import bnw.abm.intg.util.BNWProperties;
+import bnw.abm.intg.util.Log;
+
+/**
+ * @author Bhagya N. Wickramasinghe
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Log.createLogger("Synthesis","PopulationSynthesis.log");
+        BNWProperties props = null;
+        try {
+            props = new BNWProperties(args[0]);
+        } catch (Exception e) {
+            Log.info("Property file error", e);
+        }
+
+        // Read in the config properties
+        String[] sa2List = props.readCommaSepProperties("SA2List");
+        Path sa2OutputDirectory = props.readFileOrDirectoryPath("SA2OutputLocation");
+        Path allHouseholdsCsv = props.readFileOrDirectoryPath("AllHouseholdsCsv");
+        Path allFamiliesCsv = props.readFileOrDirectoryPath("AllFamiliesCsv");
+        Path allAgentsCsv = props.readFileOrDirectoryPath("AllAgentsCsv");
+        Path sa1OutputDirectory = props.readFileOrDirectoryPath("SA1OutputLocation");
+        Path sa2InputDirectory = props.readFileOrDirectoryPath("SA2InputDataDirectory");
+        Map<String, String> sa1HhDistCsvProperties = props.readKeyValuePairs("SA1HhDistFileProperties");
+        long randomSeed = Long.parseLong(props.getProperty("RandomSeed"));
+        Path saCodesZip = props.readFileOrDirectoryPath("SACodesZip");
+        String SACodesCsvInSACodesZip = props.getProperty("SACodesCsvInSACodesZip");
+        String ReferenceColumnHeader = props.getProperty("ReferenceColumnHeader");
+        String TargetColumnHeader = props.getProperty("TargetColumnHeader");
+        boolean enableSummaryReports = props.getProperty("EnableSummaryReports").trim().toLowerCase().equals("true");
+        double sexRatio = Double.parseDouble(props.getProperty("SexRatio"));
+        double relativesProbability = Double.parseDouble(props.getProperty("RelativesProbability"));
+        double femaleLoneParentProbability = Double.parseDouble(props.getProperty("FemaleLoneParentProbability"));
+        Map<String, String> ageDistributionParams = props.readKeyValuePairs("AgeDistributionFile");
+
+        long startTime = System.currentTimeMillis();
+        try {
+            Random rand = new Random(randomSeed);
+            List<Household> allHouseholds = new ArrayList<>();
+
+            for (String sa2 : sa2List) {
+                Log.info("Starting SA2: " + sa2);
+                Path hhfileinfo = Paths.get(sa2InputDirectory + File.separator + sa2 + File.separator + "Hh.csv");
+                Path indfileinfo = Paths.get(sa2InputDirectory + File.separator + sa2 + File.separator + "Indiv.csv");
+
+                GroupMaker grpmaker = new GroupMaker(sexRatio, relativesProbability, femaleLoneParentProbability);
+
+                /* Data fields */
+                Map<String, List<HhRecord>> hhrecs = null;
+                Map<String, List<IndRecord>> indrec = null;
+
+                // Read input CSVs
+                hhrecs = DataReader.readHouseholdRecords(hhfileinfo);
+                indrec = DataReader.readPersonRecords(indfileinfo);
+
+                // Build all the households (bar relationships between
+                // individuals in the HH)
+                List<Household> householdsOfSA2 = grpmaker.makePopulation(hhrecs.get(sa2), indrec.get(sa2), rand, sa2);
+
+                // Link up all the persons in each household
+                PersonPropertiesHandler.buildRelationships(householdsOfSA2, rand);
+
+                // Read in populations overall age distribution
+                Map<Integer, Double> ageDistribution = DataReader.readAgeDistribution(ageDistributionParams);
+                // Assign actual ages to persons based on their age category and
+                // overall age distribution
+                PersonPropertiesHandler.assignAge(householdsOfSA2, ageDistribution, rand);
+
+                allHouseholds.addAll(householdsOfSA2);
+
+                if (enableSummaryReports) {
+                    // Create the output directory for this SA2
+                    Path outlocationForThisSA2 = Paths.get(sa2OutputDirectory + File.separator + sa2 + File.separator);
+                    Files.createDirectories(outlocationForThisSA2);
+                    Survey.saveHouseholdSummary2csv(hhrecs.get(sa2), householdsOfSA2,
+                            Paths.get(outlocationForThisSA2 + File.separator + "GroupSummary.csv"));
+                    Survey.savePersonsSummary2csv(indrec.get(sa2), householdsOfSA2,
+                            Paths.get(outlocationForThisSA2 + File.separator + "AgentSummary.csv"));
+                    savePopulationToSA1s(sa2, sa1HhDistCsvProperties, sa2InputDirectory, sa1OutputDirectory, householdsOfSA2, rand);
+                }
+
+            }
+            convertToSA2MAINCODE(allHouseholds, saCodesZip, SACodesCsvInSACodesZip, ReferenceColumnHeader, TargetColumnHeader);
+            Log.info("Writing output files to: " + allHouseholdsCsv.getParent());
+            Files.createDirectories(allHouseholdsCsv.getParent());
+            Survey.saveAllHouseholds(allHouseholdsCsv, allHouseholds);
+            Survey.saveAllFamilies(allFamiliesCsv, allHouseholds);
+            Survey.saveAllPersons(allAgentsCsv, allHouseholds);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        double timeSpent = (System.currentTimeMillis() - startTime) / (double) 1000;
+        Log.info("Done!");
+        Log.info("Execution time: " + timeSpent);
+    }
+
+    private static void convertToSA2MAINCODE(List<Household> allHouseolds, Path zipWithCodesCsv, String csvFileInZip, String referenceColTitle,
+            String targetColTitle) throws IOException {
+        Map<String, String> sa2CodeMap = ABSStatisticalAreaCodeConverter.loadCsvAndCreateMapWithAreaCode(zipWithCodesCsv, csvFileInZip,
+                referenceColTitle, targetColTitle);
+        for (Household household : allHouseolds) {
+            household.setSA2MainCode(sa2CodeMap.get(household.getSA2Name()));
+        }
+    }
+
+    private static void savePopulationToSA1s(String thisSA2, Map<String, String> sa1HhDistCsvProperties, Path inputLocation,
+            Path sa1outputLocation, List<Household> householdsOfSA2, Random rand) throws IOException {
+        Path sa1HouseholdsFile = Paths.get(inputLocation + File.separator + thisSA2 + File.separator + sa1HhDistCsvProperties.get("FileName"));
+        int sa1Row = Integer.parseInt(sa1HhDistCsvProperties.get("SA1Row"));
+        int numberOfPersonsColumn = Integer.parseInt(sa1HhDistCsvProperties.get("NumberOfPersonsColumn"));
+        int familyHouseholdTypeColumn = Integer.parseInt(sa1HhDistCsvProperties.get("FamilyHouseholdTypeColumn"));
+        Map<String, Map<String, Integer>> sa1HouseholdCounts = null;
+        Map<String, List<Household>> householdsByType = null;
+
+        sa1HouseholdCounts = DataReader.readSA1HouseholdDistribution(sa1HouseholdsFile, sa1Row, numberOfPersonsColumn,
+                familyHouseholdTypeColumn);// FIXME: add relatives back
+        householdsByType = Survey.groupHouseholdsByHouseholdType(householdsOfSA2);
+
+        SA1PopulationMaker sa1popMaker = new SA1PopulationMaker(rand);
+        Map<String, List<Household>> householdsBySA1 = sa1popMaker.distributePopulationToSA1s(sa1HouseholdCounts, householdsByType, rand);
+        // sa1popMaker.assignTENLLDtoHouseholds(tenlldLocation, tenlldFile, householdsBySA1);
+        Survey.saveSA1Households(sa1outputLocation, householdsBySA1);
+        Survey.saveSA1Families(sa1outputLocation, householdsBySA1);
+        Survey.saveSA1Persons(sa1outputLocation, householdsBySA1);
+    }
+}
---
+++ populationbuilder/synthesis/src/main/java/bnw/abm/intg/synthesis/DataReader.java
@@ -0,0 +1,351 @@
+/**
+ *
+ */
+package bnw.abm.intg.synthesis;
+
+import bnw.abm.intg.filemanager.csv.CSVReader;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+/**
+ * @author Bhagya N. Wickramasinghe
+ */
+public class DataReader {
+
+    static Map<String, List<HhRecord>> readHouseholdRecords(Path hhfileinfo) throws IOException {
+        int titleRow = 0;
+
+        CSVParser parser = new CSVParser(Files.newBufferedReader(hhfileinfo), CSVFormat.EXCEL.withSkipHeaderRecord(false));
+        int r = -1;
+        String sa, hhSize, familyDesc, hhcount, currentSA = null;
+        int sacol = 1, hhSizecol = 2, familyDescriptioncol = 3, hhcountcol = 4;
+        List<HhRecord> hhreclist = null;
+        Map<String, List<HhRecord>> hhdata = new LinkedHashMap<>();
+        for (CSVRecord rec : parser) {
+            r++;
+            if (r <= titleRow) {
+                continue;
+            }
+
+            sa = rec.get(sacol);
+            if (!sa.equals(currentSA)) {
+                currentSA = sa;
+                hhreclist = new ArrayList<>();
+                hhdata.put(sa, hhreclist);
+            }
+
+            hhSize = rec.get(hhSizecol);
+            familyDesc = rec.get(familyDescriptioncol);
+            hhcount = rec.get(hhcountcol);
+            int nop = parseIntNofPersons(hhSize);
+            int nofFamilies = getFamilyCountInHousehold(familyDesc);
+            FamilyType primaryFamilyType = getPrimaryFamilyType(familyDesc);
+            int nofHhs = Integer.parseInt(hhcount);
+            HhRecord hhr = new HhRecord(nop, nofFamilies, primaryFamilyType, nofHhs);
+            hhreclist.add(hhr);
+        }
+        parser.close();
+
+        return hhdata;
+    }
+
+    static Map<String, List<IndRecord>> readPersonRecords(Path indfileinfo) throws IOException {
+        int titleRow = 0;
+        CSVParser parser = new CSVParser(Files.newBufferedReader(indfileinfo), CSVFormat.EXCEL.withSkipHeaderRecord(false));
+        int r = -1;
+        String sa, ageRangeStr, currentSA = null;
+        PersonType personType;
+        ChildType childType;
+        Sex sex;
+        int sacol = 1, relcol = 2, sexcol = 3, agecol = 4, nofagentscol = 5;
+        List<IndRecord> indreclist = new ArrayList<>();
+        Map<String, List<IndRecord>> inddata = new LinkedHashMap<>();
+        for (CSVRecord rec : parser) {
+            r++;
+            if (r <= titleRow) {
+                continue;
+            }
+            sa = rec.get(sacol);
+            if (!sa.equals(currentSA)) {
+                currentSA = sa;
+                indreclist = new ArrayList<>();
+                inddata.put(sa, indreclist);
+            }
+
+            personType = getPersonType(rec.get(relcol));
+            if (personType == PersonType.Child)
+                childType = getChildType(rec.get(relcol));
+            else
+                childType = null;
+            sex = getSex(rec.get(sexcol));
+            ageRangeStr = rec.get(agecol);
+
+            int personcount = Integer.parseInt(rec.get(nofagentscol));
+            IndRecord indr = new IndRecord(personType, childType, sex, getAgeRange(ageRangeStr), personcount);
+            indreclist.add(indr);
+        }
+        parser.close();
+        return inddata;
+    }
+
+    static Map<String, Map<String, Integer>> readSA1HouseholdDistribution(Path csvFile, int sa1Row, int numberOfPersonsCol, int familyHhTypeCol)
+            throws IOException {
+        CSVParser csvParser = new CSVParser(Files.newBufferedReader(csvFile), CSVFormat.EXCEL.withSkipHeaderRecord(false));
+        int row = -1;
+
+        Map<String, Map<String, Integer>> householdTypesBySA1 = new LinkedHashMap<>();
+        List<String> sa1list = new ArrayList<>();
+
+        for (CSVRecord csvRecord : csvParser) {
+            row++;
+            if (row == sa1Row) {
+                for (int column = 0; column < csvRecord.size(); column++) {
+                    sa1list.add(csvRecord.get(column));
+                }
+            } else if (row > sa1Row) {
+                int numberOfPersons = parseIntNofPersons(csvRecord.get(numberOfPersonsCol));
+                int familyCount = getFamilyCountInHousehold(csvRecord.get(familyHhTypeCol));
+                FamilyType primaryFamily = getPrimaryFamilyType(csvRecord.get(familyHhTypeCol));
+
+                String key = numberOfPersons + ":" + familyCount + ":" + primaryFamily;
+
+                Map<String, Integer> sa1Map = new LinkedHashMap<>();
+                for (int column = 0; column < csvRecord.size(); column++) {
+                    if (column > familyHhTypeCol) {
+                        sa1Map.put(sa1list.get(column), Integer.parseInt(csvRecord.get(column)));
+                    }
+                }
+                householdTypesBySA1.put(key, sa1Map);
+            }
+
+        }
+        csvParser.close();
+
+        return householdTypesBySA1;
+    }
+
+    static Map<Integer, Double> readAgeDistribution(Map<String, String> params) throws IOException {
+        Path csvFile = Paths.get(params.get("FileName"));
+        CSVParser csvParser = new CSVParser(Files.newBufferedReader(csvFile), CSVFormat.EXCEL.withSkipHeaderRecord(false));
+        int row = -1;
+        int dataRow = Integer.parseInt(params.get("DataStartRow"));
+        int percentageCol = Integer.parseInt(params.get("PercentageColumn"));
+        int ageColumn = Integer.parseInt(params.get("AgeColumn"));
+
+        Map<Integer, Double> ageDist = new LinkedHashMap<>();
+        for (CSVRecord csvRecord : csvParser) {
+            row++;
+            if (row >= dataRow) {
+                if ((csvRecord.size() - 1) < ageColumn || csvRecord.get(ageColumn) == null || csvRecord.get(ageColumn).equals("")) {
+                    break; // We have reached end
+                }
+                String age = csvRecord.get(ageColumn).split(" ")[0];
+                ageDist.put(Integer.parseInt(age), Double.parseDouble(csvRecord.get(percentageCol)));
+
+            }
+
+        }
+        csvParser.close();
+        return ageDist;
+    }
+
+    static List<List<String>> readTenlldDistribution(Path csvFile) throws IOException {
+        CSVReader csvReader = new CSVReader(CSVFormat.EXCEL.withSkipHeaderRecord(false));
+        csvReader.setStripChars(new String[]{"{", "}", "B"});
+        List<List<String>> data = csvReader.readCsvRows(Files.newBufferedReader(csvFile));
+        return data;
+
+    }
+
+    private static Sex getSex(String sexDescription) {
+        switch (sexDescription) {
+            case "Male":
+                return Sex.Male;
+            case "Female":
+                return Sex.Female;
+            default:
+                throw new Error("Unrecognised sex");
+        }
+    }
+
+    private static PersonType getPersonType(String relationshipType) {
+        PersonType ptype;
+        switch (relationshipType) {
+            case "Relatives":
+                ptype = PersonType.Relative;
+                break;
+            case "Married":
+                ptype = PersonType.Married;
+                break;
+            case "Lone parent":
+                ptype = PersonType.LoneParent;
+                break;
+            case "U15Child":
+                ptype = PersonType.Child;
+                break;
+            case "Student":
+                ptype = PersonType.Child;
+                break;
+            case "O15Child":
+                ptype = PersonType.Child;
+                break;
+            case "GroupHhold":
+                ptype = PersonType.GroupHousehold;
+                break;
+            case "Lone person":
+                ptype = PersonType.LonePerson;
+                break;
+            default:
+                throw new Error("Unrecognised Person type");
+        }
+        return ptype;
+    }
+
+    private static ChildType getChildType(String relationshipType) {
+        ChildType childType;
+        switch (relationshipType) {
+            case "U15Child":
+                childType = ChildType.U15Child;
+                break;
+            case "Student":
+                childType = ChildType.Student;
+                break;
+            case "O15Child":
+                childType = ChildType.O15Child;
+                break;
+            default:
+                throw new Error("Unrecognised Child type");
+        }
+        return childType;
+    }
+
+    private static int parseIntNofPersons(String nofPersons) throws IOException {
+        int nop = 0;
+        switch (nofPersons) {
+            case "One person":
+                nop = 1;
+                break;
+            case "Two persons":
+                nop = 2;
+                break;
+            case "Three persons":
+                nop = 3;
+                break;
+            case "Four persons":
+                nop = 4;
+                break;
+            case "Five persons":
+                nop = 5;
+                break;
+            case "Six persons":
+                nop = 6;
+                break;
+            case "Seven persons":
+                nop = 7;
+                break;
+            case "Eight or more persons":
+                nop = 8;
+                break;
+            default:
+                throw new IOException("Unrecognised Number of persons in dwelling (NPRD) value: " + nofPersons);
+        }
+        return nop;
+    }
+
+    private static AgeRange getAgeRange(String ageRangeStr) {
+        AgeRange age = null;
+        switch (ageRangeStr) {
+            case "0-14 years":
+                age = AgeRange.A0_14;
+                break;
+            case "15-24 years":
+                age = AgeRange.A15_24;
+                break;
+            case "25-39 years":
+                age = AgeRange.A25_39;
+                break;
+            case "40-54 years":
+                age = AgeRange.A40_54;
+                break;
+            case "55-69 years":
+                age = AgeRange.A55_69;
+                break;
+            case "70-84 years":
+                age = AgeRange.A70_84;
+                break;
+            case "85-99 years":
+                age = AgeRange.A85_99;
+                break;
+            case "100 years and over":
+                age = AgeRange.A100_110;
+                break;
+            default:
+                throw new Error("Unrecognised age range: " + ageRangeStr);
+        }
+        return age;
+    }
+
+    private static FamilyType getPrimaryFamilyType(String familyDescription) {
+        for (FamilyType familyType : EnumSet.allOf(FamilyType.class)) {
+            if (familyDescription.contains(familyType.description())) {
+                return familyType;
+            }
+        }
+        throw new Error("Primary family type could not be recognised");
+    }
+
+    private static int getFamilyCountInHousehold(String familyDescription) {
+        if (familyDescription.contains("One family")) {
+            return 1;
+        } else if (familyDescription.contains("Two family")) {
+            return 2;
+        } else if (familyDescription.contains("Three or more family")) {
+            return 3;
+        } else if (familyDescription.contains("Group household") | familyDescription.contains("Lone person")) {
+            return 1;
+        }
+
+        throw new Error("Number of families could not be recognised");
+    }
+
+}
+
+class HhRecord {
+    final public int numOfPersonsPerHh;
+    final public int hhCount;
+    final public FamilyType primaryFamilyType;
+    final public int familyCountPerHousehold;
+
+    public HhRecord(int nofPersons, int familyCountPerHh, FamilyType familyType, int hhcount) {
+        this.numOfPersonsPerHh = nofPersons;
+        this.familyCountPerHousehold = familyCountPerHh;
+        this.hhCount = hhcount;
+        this.primaryFamilyType = familyType;
+    }
+}
+
+class IndRecord {
+    final public PersonType personType;
+    final public ChildType childType;
+    final public Sex sex;
+    final public AgeRange ageRange;
+    final public int indCount;
+
+    public IndRecord(PersonType type, ChildType childType, Sex sex, AgeRange agerange, int individualscount) {
+        this.ageRange = agerange;
+        this.sex = sex;
+        this.personType = type;
+        this.childType = childType;
+        this.indCount = individualscount;
+        if (individualscount > 0) {
+            this.ageRange.markNotEmpty(true);
+        }
+    }
+}
