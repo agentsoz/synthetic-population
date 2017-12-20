5919638c83a125f8db991de5c0039541716f839c
wniroshan <s3467727@student.rmit.edu.au>
2017-12-20 20:30:59 +1100

    not increasing married males to match married females and refactoring

--- datapreprocessing/cleaning.R
+++ datapreprocessing/cleaning.R
@@ -39,6 +39,23 @@ smart.round <- function(x) {
 }
 
 
+#' Title
+#'
+#' @param person 
+#' @param pSAColi 
+#' @param pRelColi 
+#' @param pSexColi 
+#' @param pAgeColi 
+#' @param pValColi 
+#' @param hhold 
+#' @param hPersonCountColi 
+#' @param hFamilyTypeColi 
+#' @param hValColi 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 cleanup <- function(person, pSAColi, pRelColi, pSexColi, pAgeColi,pValColi, hhold, hPersonCountColi, hFamilyTypeColi, hValColi){
   
   person[,pValColi] <- as.numeric(person[,pValColi])
@@ -101,18 +118,18 @@ cleanup <- function(person, pSAColi, pRelColi, pSexColi, pAgeColi,pValColi, hhol
   ttlgrphh = sum(hhold[grpHhsrwids,hValColi]*seq(1,8))
   cat("Group households: total persons in households file",ttlgrphh,"\n")
   
-  pdiff = ttlgrphh - ttlgrpin
-  if(pdiff > 0){
-    percent = pdiff/ttlgrpin*100
-    person[grpIndrwids,pValColi] = fillAccording2Dist(person[grpIndrwids,pValColi], pdiff)
-    cat("Group households: less persons than households, adding new agents:", pdiff,"(",percent,"%)\n")
-  }else if(pdiff == 0){
+  diff = ttlgrphh - ttlgrpin
+  if(diff > 0){
+    percent = diff/ttlgrpin*100
+    person[grpIndrwids,pValColi] = fillAccording2Dist(person[grpIndrwids,pValColi], diff)
+    cat("Group households: less persons than households, adding new agents:", diff,"(",percent,"%)\n")
+  }else if(diff == 0){
     cat("Group households: No difference\n")
   }else{
-    percent = pdiff/ttlgrpin*100
-    cat("Group households: more persons than households, removing extra agents", pdiff,"(",percent,"%)\n")
-    person[grpIndrwids,pValColi] = fillAccording2Dist(person[grpIndrwids,pValColi], pdiff)
-    extra = extra + pdiff
+    percent = diff/ttlgrpin*100
+    cat("Group households: more persons than households, removing extra agents", diff,"(",percent,"%)\n")
+    person[grpIndrwids,pValColi] = fillAccording2Dist(person[grpIndrwids,pValColi], diff)
+    extra = extra + diff
   }
   
   #Check lone person households
@@ -122,119 +139,100 @@ cleanup <- function(person, pSAColi, pRelColi, pSexColi, pAgeColi,pValColi, hhol
   lnpersonhhsrwids = getMatchingRowIds(hhold, hFamilyTypeColi,"Lone person household")
   ttllnpersonhhs = sum(hhold[lnpersonhhsrwids,hValColi])
   cat("Lone persons: total persons in household files",ttllnpersonhhs,"\n")
-  pdiff = ttllnpersonhhs - ttllnpersons
-  if(pdiff >0){
-    percent = pdiff/ttllnpersons*100
-    person[lnpersonrwids,pValColi]= fillAccording2Dist(person[lnpersonrwids,pValColi],pdiff)
-    cat("Lone person: less persons than households, adding new agents:", pdiff,"(",percent,"%)\n")
-  }else if(pdiff == 0){
+  diff = ttllnpersonhhs - ttllnpersons
+  if(diff >0){
+    percent = diff/ttllnpersons*100
+    person[lnpersonrwids,pValColi]= fillAccording2Dist(person[lnpersonrwids,pValColi],diff)
+    cat("Lone person: less persons than households, adding new agents:", diff,"(",percent,"%)\n")
+  }else if(diff == 0){
     print("Lone person: No difference")
   }else{
-    percent = pdiff/ttllnpersonhhs*100
-    cat("Lone persons: more persons than households, removing extra agents:", pdiff,"(",percent,"%)\n")
-    person[lnpersonrwids,pValColi]= fillAccording2Dist(person[lnpersonrwids,pValColi],pdiff)
-    extra = extra + pdiff
+    percent = diff/ttllnpersonhhs*100
+    cat("Lone persons: more persons than households, removing extra agents:", diff,"(",percent,"%)\n")
+    person[lnpersonrwids,pValColi]= fillAccording2Dist(person[lnpersonrwids,pValColi],diff)
+    extra = extra + diff
   }
   
   #Match married males to married females
-  marriedrwids = getMatchingRowIds(person, pRelColi, "Married")
-  marMalerwids = getMatchingRowIds(person[marriedrwids,], pSexColi, "Male")
-  marFemalerwids = getMatchingRowIds(person[marriedrwids,], pSexColi,"Female")
-  ttlMarriedMales = sum(person[marMalerwids,pValColi])
+  marriedRwIds = getMatchingRowIds(person, pRelColi, "Married")
+  marMaleRwIds = getMatchingRowIds(person[marriedRwIds,], pSexColi, "Male")
+  marFemaleRwIds = getMatchingRowIds(person[marriedRwIds,], pSexColi,"Female")
+  ttlMarriedMales = sum(person[marMaleRwIds,pValColi])
   cat("Married couples: total married males in persons file:",ttlMarriedMales,"\n")
-  ttlMarriedFemales = sum(person[marFemalerwids,pValColi])
+  ttlMarriedFemales = sum(person[marFemaleRwIds,pValColi])
   cat("Married couples: total married females in persons file:",ttlMarriedFemales,"\n")
   
-  f1cplonlyrwids = getMatchingRowIds(hhold, hFamilyTypeColi, "One family household: Couple family with no children")
-  f2cplonlyrwids =getMatchingRowIds(hhold, hFamilyTypeColi, "Two family household: Couple family with no children")
-  f3cplonlyrwids =getMatchingRowIds(hhold, hFamilyTypeColi, "Three or more family household: Couple family with no children")
+  f1CplOnlyRwIds = getMatchingRowIds(hhold, hFamilyTypeColi, "One family household: Couple family with no children")
+  f2CplOnlyRwIds =getMatchingRowIds(hhold, hFamilyTypeColi, "Two family household: Couple family with no children")
+  f3CplOnlyRwIds =getMatchingRowIds(hhold, hFamilyTypeColi, "Three or more family household: Couple family with no children")
   
-  f1cplYschildrwids = getMatchingRowIds(hhold, hFamilyTypeColi, "One family household: Couple family with children")
-  f2cplYschildrwids =getMatchingRowIds(hhold, hFamilyTypeColi, "Two family household: Couple family with children")
-  f3cplYschildrwids =getMatchingRowIds(hhold, hFamilyTypeColi, "Three or more family household: Couple family with children")
+  f1CplYsChildRwIds = getMatchingRowIds(hhold, hFamilyTypeColi, "One family household: Couple family with children")
+  f2CplYsChildRwIds =getMatchingRowIds(hhold, hFamilyTypeColi, "Two family household: Couple family with children")
+  f3CplYsChildRwIds =getMatchingRowIds(hhold, hFamilyTypeColi, "Three or more family household: Couple family with children")
   
-  sumcplonly = sum(hhold[f1cplonlyrwids,hValColi],hhold[f2cplonlyrwids,hValColi],hhold[f3cplonlyrwids,hValColi])
-  cat("Married couples: total couple families with no children in household file:",sumcplonly,"\n")
-  sumcplyschld = sum(hhold[f1cplYschildrwids,hValColi],hhold[f2cplYschildrwids,hValColi],hhold[f3cplYschildrwids,hValColi])
-  cat("Married couples: total couple families with children in household file:",sumcplyschld,"\n")
-  mincpls = sum(sumcplyschld,sumcplonly)
-  cat("Married couples: minimum required number of couples:",mincpls,"\n")
+  sumCplOnly = sum(hhold[f1CplOnlyRwIds,hValColi],hhold[f2CplOnlyRwIds,hValColi],hhold[f3CplOnlyRwIds,hValColi])
+  cat("Married couples: total couple families with no children in household file:",sumCplOnly,"\n")
+  sumCplYsChld = sum(hhold[f1CplYsChildRwIds,hValColi],hhold[f2CplYsChildRwIds,hValColi],hhold[f3CplYsChildRwIds,hValColi])
+  cat("Married couples: total couple families with children in household file:",sumCplYsChld,"\n")
+  minRequiredCpls = sum(sumCplYsChld,sumCplOnly)
+  cat("Married couples: minimum required number of couples:",minRequiredCpls,"\n")
   
-  if(ttlMarriedFemales >= ttlMarriedMales){
-    if(ttlMarriedFemales > ttlMarriedMales){
-      cat("Married Couples: more married females than married males\n")
-    }else{
-      cat("Married Couples: married females equal to married males\n")
-    }
-    pdiff = mincpls - ttlMarriedFemales
-    if(pdiff > 0){
-      percent = pdiff/ttlMarriedFemales *100
-      person[marFemalerwids,pValColi] = fillAccording2Dist(person[marFemalerwids,pValColi], pdiff)
-      cat("Married Couples: there are not enough married females to form min required couples, increase married females by :", pdiff,"(",percent,"%)\n")
-    }else if(pdiff ==0){
-      cat("Married Couples: married females are equal to couple families\n")
-    }else{
-      cat("Married Couples: there are more married females than couple families, no problem\n")
-    }
-    ttlMarriedFemales =sum(person[marFemalerwids,pValColi])
-    pdiff = ttlMarriedFemales - ttlMarriedMales
-    if(pdiff > 0){
-      percent =  pdiff/ttlMarriedMales*100
-      cat("Married Couples: increase married males to match married females:",pdiff,"(",percent,"%)\n")  
-      person[marMalerwids,pValColi] =  fillAccording2Dist(person[marMalerwids,pValColi], pdiff)
-    }
-  }else {
-    cat("Married Couples: more married males than married females\n")
-    pdiff = mincpls - ttlMarriedMales
-    if(pdiff > 0){
-      percent =  pdiff/ttlMarriedMales*100
-      person[marMalerwids,pValColi] = fillAccording2Dist(person[marMalerwids,pValColi], pdiff)
-      cat("Marred Couples: there are not there are enough married males to form min required couples, increase married males by :", pdiff,"(",percent,"%)\n")
-    }else if(pdiff ==0){
-      cat("Married Couples: married males are equal to couple families\n")
-    }
-    ttlMarriedMales =sum(person[marMalerwids,pValColi])
-    pdiff = ttlMarriedMales -ttlMarriedFemales
-    if(pdiff > 0){
-      percent =  pdiff/ttlMarriedFemales*100
-      cat("Married Couples: increase Married females to match married males by:",pdiff,"(",percent,"%)\n")  
-      person[marFemalerwids,pValColi] =  fillAccording2Dist(person[marFemalerwids,pValColi], pdiff)
-    }
+  diff = minRequiredCpls - ttlMarriedFemales
+  if(diff > 0){
+    percent = diff/ttlMarriedFemales *100
+    person[marFemaleRwIds,pValColi] = fillAccording2Dist(person[marFemaleRwIds,pValColi], diff)
+    cat("Married Couples: there are not enough married females to form min required couples, increase married females by :", diff,"(",percent,"%)\n")
+  }else if(diff ==0){
+    cat("Married Couples: married females are equal to couple families\n")
+  }else{
+    cat("Married Couples: there are more married females than couple families, no problem\n")
+  }
+  
+  diff = minRequiredCpls - ttlMarriedMales
+  if(diff > 0){
+    percent =  diff/ttlMarriedMales*100
+    person[marMaleRwIds,pValColi] = fillAccording2Dist(person[marMaleRwIds,pValColi], diff)
+    cat("Marred Couples: there are not there are enough married males to form min required couples, increase married males by :", diff,"(",percent,"%)\n")
+  }else if(diff ==0){
+    cat("Married Couples: married males are equal to couple families\n")
+  }else{
+    cat("Married Couples: there are more married males than couple families, no problem\n")
   }
   
   #Check Lone Parents
-  f1loneparentrwids = getMatchingRowIds(hhold, hFamilyTypeColi,"One family household: One parent family")
-  f2loneparentrwids = getMatchingRowIds(hhold, hFamilyTypeColi,"Two family household: One parent family")
-  f3loneparentrwids = getMatchingRowIds(hhold, hFamilyTypeColi,"Three or more family household: One parent family")
-  ttloneparentfamilies = sum(hhold[f1loneparentrwids,hValColi],hhold[f2loneparentrwids,hValColi],hhold[f3loneparentrwids,hValColi])
-  cat("Lone Parents: minimum required number of lone parents in households file",ttloneparentfamilies,"\n")
-  oneparentrwids = getMatchingRowIds(person, pRelColi, "Lone parent")
-  ttloneparents = sum(person[oneparentrwids,pValColi])
-  cat("Lone Parents: lone parents in persons file",ttloneparents,"\n")
-  pdiff = ttloneparentfamilies - ttloneparents
-  if(pdiff > 0){
-    percent = pdiff/ttloneparents*100
-    person[oneparentrwids,pValColi] = fillAccording2Dist(person[oneparentrwids,pValColi], pdiff)
-    cat("Lone Parents: less lone parents than required by families, adding new agents",pdiff,"(",percent,"%)\n")
+  f1OneParentRwIds = getMatchingRowIds(hhold, hFamilyTypeColi,"One family household: One parent family")
+  f2OneParentRwIds = getMatchingRowIds(hhold, hFamilyTypeColi,"Two family household: One parent family")
+  f3OneParentRwids = getMatchingRowIds(hhold, hFamilyTypeColi,"Three or more family household: One parent family")
+  ttlOneParentFamilies = sum(hhold[f1OneParentRwIds,hValColi],hhold[f2OneParentRwIds,hValColi],hhold[f3OneParentRwids,hValColi])
+  cat("Lone Parents: minimum required number of lone parents in households file",ttlOneParentFamilies,"\n")
+  
+  loneParentRwIds = getMatchingRowIds(person, pRelColi, "Lone parent")
+  ttlLoneparents = sum(person[loneParentRwIds,pValColi])
+  cat("Lone Parents: lone parents in persons file",ttlLoneparents,"\n")
+  diff = ttlOneParentFamilies - ttlLoneparents
+  if(diff > 0){
+    percent = diff/ttlLoneparents*100
+    person[loneParentRwIds,pValColi] = fillAccording2Dist(person[loneParentRwIds,pValColi], diff)
+    cat("Lone Parents: less lone parents than required by families, increasing lone parents by ",diff," (",percent,"%)\n")
   }else{
     cat("Lone Parents: more lone parents in persons file than Lone parent primary families in households file, no problem\n")
   }
   
   #Check children
-  u15rwids = getMatchingRowIds(person, pRelColi, "U15Child")
-  sturwids = getMatchingRowIds(person, pRelColi, "Student")
-  o15rwids = getMatchingRowIds(person, pRelColi, "O15Child")
-  ttlchlds = sum(person[u15rwids,pValColi], person[sturwids,pValColi],person[o15rwids,pValColi])
-  cat("Children: total children (U15 + Student + O15) in persons file:",ttlchlds,"\n")
+  u15RwIds = getMatchingRowIds(person, pRelColi, "U15Child")
+  stuRwIds = getMatchingRowIds(person, pRelColi, "Student")
+  o15RwIds = getMatchingRowIds(person, pRelColi, "O15Child")
+  ttlChlds = sum(person[u15RwIds,pValColi], person[stuRwIds,pValColi],person[o15RwIds,pValColi])
+  cat("Children: total children (U15 + Student + O15) in persons file:",ttlChlds,"\n")
   
-  ttlFamiliesWithChildren = ttloneparentfamilies+sumcplyschld
+  ttlFamiliesWithChildren = ttlOneParentFamilies+sumCplYsChld
   cat("Children: total familes with children (One parent + Couple with children) in households file:",ttlFamiliesWithChildren,"\n")
   
-  pdiff = ttlFamiliesWithChildren - ttlchlds
-  if(pdiff > 0){
-    percent = pdiff/ttlchlds*100
-    person[c(u15rwids,sturwids,o15rwids),pValColi] = fillAccording2Dist(person[c(u15rwids,sturwids,o15rwids),pValColi], pdiff)
-    cat("Children: less children than families, adding new agents:", pdiff,"(",percent,"%)\n")
+  diff = ttlFamiliesWithChildren - ttlChlds
+  if(diff > 0){
+    percent = diff/ttlChlds*100
+    person[c(u15RwIds,stuRwIds,o15RwIds),pValColi] = fillAccording2Dist(person[c(u15RwIds,stuRwIds,o15RwIds),pValColi], diff)
+    cat("Children: less children than families, adding new agents:", diff,"(",percent,"%)\n")
   }else{
     cat("Children: there are enough children to construct all known basic family structures with children, no problem\n")
   }
@@ -256,11 +254,11 @@ cleanup <- function(person, pSAColi, pRelColi, pSexColi, pAgeColi,pValColi, hhol
   hhrequiredreltives = minRelativesFor2fn3f + reltivsFor1FOtherfamily
   cat("Relatives for Other families: minimum required relatives to form all primary Other familes:",hhrequiredreltives,"\n")
   
-  pdiff = hhrequiredreltives - ttlrelatives
-  if(pdiff > 0){
-    percent = pdiff/ttlrelatives*100
-    person[relrwids,pValColi] = fillAccording2Dist(person[relrwids,pValColi], pdiff)
-    cat("Relatives for Other families: less persons than households, adding new agents:", pdiff,"(",percent,"%)\n")
+  diff = hhrequiredreltives - ttlrelatives
+  if(diff > 0){
+    percent = diff/ttlrelatives*100
+    person[relrwids,pValColi] = fillAccording2Dist(person[relrwids,pValColi], diff)
+    cat("Relatives for Other families: less persons than households, adding new agents:", diff,"(",percent,"%)\n")
   }else{
     cat("Relatives for Other families: there are enough relatives to construch all basic other family structures\n")
   }
