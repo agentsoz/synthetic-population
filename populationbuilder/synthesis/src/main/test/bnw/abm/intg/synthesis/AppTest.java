bdb236b2934399d52365ee86ba60bf19637745d3
Bhagya Wickramasinghe <wniroshan@gmail.com>
2018-03-26 12:09:53 +1100

    travis build should fail

---
+++ populationbuilder/synthesis/src/main/test/bnw/abm/intg/synthesis/AppTest.java
@@ -0,0 +1,14 @@
+package bnw.abm.intg.synthesis;
+
+import org.junit.Assert;
+import org.junit.Test;
+/**
+ * @author wniroshan 26 Mar 2018
+ */
+public class AppTest {
+
+    @Test
+    public void testSetupVerification(){
+        Assert.assertEquals("Junit test setup", 1,0);
+    }
+}
