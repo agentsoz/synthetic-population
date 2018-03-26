df038362e4bc012c1dda316344bae2b4e5c55279
Bhagya Wickramasinghe <wniroshan@gmail.com>
2018-03-26 12:36:05 +1100

    Test expected to fail

--- populationbuilder/synthesis/src/main/test/bnw/abm/intg/synthesis/AppTest.java
+++ populationbuilder/synthesis/src/main/test/bnw/abm/intg/synthesis/AppTest.java
@@ -9,6 +9,6 @@ public class AppTest {
 
     @Test
     public void testSetupVerification(){
-        Assert.assertEquals("Junit test setup", 1,1);
+        Assert.assertEquals("Junit test setup", 1,0);
     }
 }
