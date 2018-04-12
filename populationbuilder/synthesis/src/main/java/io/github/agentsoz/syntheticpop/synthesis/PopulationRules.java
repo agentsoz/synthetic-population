4868fcdb66cdbadcecb60aa5523b2c6526a61267
Bhagya Wickramasinghe <wniroshan@gmail.com>
2018-04-13 00:17:39 +1000

    improving parent-child age gap rule validation

--- populationbuilder/synthesis/src/main/java/io/github/agentsoz/syntheticpop/synthesis/PopulationRules.java
+++ populationbuilder/synthesis/src/main/java/io/github/agentsoz/syntheticpop/synthesis/PopulationRules.java
@@ -1,6 +1,9 @@
 package io.github.agentsoz.syntheticpop.synthesis;
 
 import io.github.agentsoz.syntheticpop.synthesis.models.AgeRange;
+import io.github.agentsoz.syntheticpop.synthesis.models.Person;
+
+import java.util.function.Function;
 
 /**
  * @author wniroshan 11 Jan 2018
@@ -8,34 +11,47 @@ import io.github.agentsoz.syntheticpop.synthesis.models.AgeRange;
 public class PopulationRules {
 
     /**
-     * Validates the rule that a child must come from an age category between 15 to 45 years age gap (younger) to the parent's.
+     * Validates the rule that a child must come from an age category between 15 to 60 years age gap (younger) to the parent's.
      *
-     * @param parent1Age The AgeRange of the parent1
-     * @param parent2Age The AgeRange of parent2
-     * @param childAge   The AgeRange of the child
+     * @param parent1 The first parent. Can be a Person or an AgeRange.
+     * @param parent2 The second parent. Can be a Person or an AgeRange
+     * @param child   The age of the child. Can be a Person, an AgeRange or an age (integer)
      * @return True if age ranges are valid, otherwise false
      */
-    public static boolean validateParentChildAgeRule(AgeRange parent1Age, AgeRange parent2Age, AgeRange childAge) {
-        int parent1AgeGap = parent1Age.min() - childAge.max();
-        boolean parent2AgeCondition, parent1AgeCondition = 1 <= parent1AgeGap && parent1AgeGap <= 46; //The effective age gap is 45
-        if (parent2Age != null) {
-            int parent2AgeGap = parent2Age.min() - childAge.max();
-            parent2AgeCondition = 1 <= parent2AgeGap && parent2AgeGap <= 46; //The effective age gap is 45
-        } else {
-            parent2AgeCondition = true;
-        }
-        return parent1AgeCondition & parent2AgeCondition;
-    }
+    public static <T, E> boolean validateParentChildAgeRule(T parent1, T parent2, E child) {
+        int childAge;
 
-    /**
-     * Validates the rule that a child must come from an age category with at least a 15 year age gap (younger) to the parent's.
-     *
-     * @param parentAge The age of the parent
-     * @param childAge  The age of the child
-     * @return True if age ranges are valid, otherwise false
-     */
-    public static boolean validateParentChildAgeRule(int parentAge, int childAge) {
-        int ageGap = parentAge - childAge;
-        return 15 <= ageGap && ageGap <= 60;
+        if (child instanceof AgeRange) {
+            childAge = ((AgeRange) child).min();
+        } else if (child instanceof Integer) {
+            childAge = (Integer)child;
+        } else if (child instanceof Person) {
+            childAge = ((Person) child).getAge() >= 0 ? ((Person) child).getAge() : ((Person) child).getAgeRange().min();
+        } else {
+            throw new Error(child.getClass() + " is not a compatible age representation");
+        }
+
+        Function<T, Boolean> validateAgeGap = (T parent) -> {
+            boolean parentAgeCondition;
+            if (parent != null) {
+                int parentAge;
+                if (parent instanceof Person) {
+                    parentAge = (((Person) parent).getAge() >= 0) ? ((Person) parent).getAge() : ((Person) parent).getAgeRange().min();
+                } else if (parent instanceof AgeRange) {
+                    parentAge = ((AgeRange) parent).min();
+                } else if (parent instanceof Integer) {
+                    parentAge = (Integer) parent;
+                } else {
+                    throw new Error(parent.getClass() + " is not a compatible age representation");
+                }
+                int parent1AgeGap = parentAge - childAge;
+                parentAgeCondition = 1 <= parent1AgeGap && parent1AgeGap <= 60;
+            } else {
+                parentAgeCondition = true;
+            }
+            return parentAgeCondition;
+        };
+
+        return validateAgeGap.apply(parent1) && validateAgeGap.apply(parent2);
     }
 }
