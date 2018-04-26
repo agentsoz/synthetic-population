c74e91c87a4aa095ad7a461b95cb8da0992f27e4
wniroshan <s3467727@student.rmit.edu.au>
2018-04-26 12:30:44 +1000

    Creating a zip file with multiple direcotiries and refactoring

--- populationbuilder/filemanager/src/main/java/io/github/agentsoz/syntheticpop/filemanager/zip/Zip.java
+++ populationbuilder/filemanager/src/main/java/io/github/agentsoz/syntheticpop/filemanager/zip/Zip.java
@@ -1,75 +1,130 @@
 /**
- * 
+ *
  */
 package io.github.agentsoz.syntheticpop.filemanager.zip;
 
 import io.github.agentsoz.syntheticpop.filemanager.Find;
+import io.github.agentsoz.syntheticpop.util.LambdaCheckedException;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Reader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 
 /**
  * @author Bhagya N. Wickramasinghe
- *
  */
 public class Zip {
 
-	public static Reader read(Path zipFile, String fileInZip) throws IOException {
-		FileSystem fs = FileSystems.newFileSystem(zipFile, null);
-		return Files.newBufferedReader(fs.getPath(fileInZip));
-	}
+    /**
+     * Returns a java.io.Reader for the specifed file in the zipFile
+     * @param zipFile The path to the zipFile
+     * @param fileInZip The file inside the zip file
+     * @return A reader instance for the file in the zip
+     * @throws IOException If a Reader instance cannot be instantiated for the specified file
+     */
+    public static Reader read(Path zipFile, String fileInZip) throws IOException {
+        FileSystem fs = FileSystems.newFileSystem(zipFile, null);
+        return Files.newBufferedReader(fs.getPath(fileInZip));
+    }
 
-	public static void create(Path zipFile, List<Path> componentFiles) throws IOException, URISyntaxException {
-		Map<String, String> env = new HashMap<>();
-		env.put("create", "true");
-		// locate file system by using the syntax
-		// defined in java.net.JarURLConnection
-		URI uri = new URI("jar:" + zipFile.toUri());
+    /**
+     * /** Creates a zip file with the specified files
+     *
+     * @param zipFile        The output file
+     * @param componentFiles The files to add to the zip file
+     * @throws IOException        When reading and writing files
+     * @throws URISyntaxException If the URI of the zipFile cannot be obtained
+     */
+    public static void archiveFiles(Path zipFile, List<Path> componentFiles) throws IOException, URISyntaxException {
+        Map<String, String> env = new HashMap<>();
+        env.put("create", "true");
+        // locate file system by using the syntax
+        // defined in java.net.JarURLConnection
+        URI uri = new URI("jar:" + zipFile.toUri());
 
-		try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
-			for (Path externalFilePath : componentFiles) {
-				Path filePathInZip = zipfs.getPath(externalFilePath.getFileName().toString());
-				Files.copy(externalFilePath, filePathInZip, StandardCopyOption.REPLACE_EXISTING);
-			}
-		}
-	}
+        try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
+            for (Path externalFilePath : componentFiles) {
+                Path filePathInZip = zipfs.getPath(externalFilePath.getFileName().toString());
+                Files.copy(externalFilePath, filePathInZip, StandardCopyOption.REPLACE_EXISTING);
+            }
+        }
+    }
 
-	public static List<Path> findFiles(Path jarFile, String pattern) throws IOException {
-		
-		Path currDir = Paths.get("").toAbsolutePath();
-		jarFile = currDir.resolve(jarFile);
-		
-		HashMap<Path, FileSystem> fileSystems = new HashMap<>();
-		FileSystem fs = fileSystems.get(jarFile);
-		if (fs == null) {
-			// throw new FileNotFoundException(jarFile.toString());
-			Map<String, String> env = new HashMap<>();
-			env.put("create", "false");
-			try {
-				fs = FileSystems.newFileSystem(URI.create("jar:file:" + jarFile), env);
-			} catch (FileSystemNotFoundException iae) {
-				throw new FileNotFoundException(jarFile.toString());
-			}
-			fileSystems.put(jarFile, fs);
-		}
+    /**
+     * Creates a zip file with the specified directories and files in them
+     *
+     * @param zipFile        The output file
+     * @param directories    The directories to add to the zip file
+     * @param deleteOriginal Set true to delete the original direcories
+     * @throws IOException When reading and writing files
+     */
+    public static void archiveDirectories(Path zipFile, List<Path> directories, boolean deleteOriginal) throws IOException {
+        if (Files.exists(zipFile)) {
+            Files.delete(zipFile);
+        }
+        Files.createFile(zipFile);
 
-		Find finder = new Find(pattern, fs);
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            for (Path sourcePath : directories) {
+                Files.walk(sourcePath)
+                     .filter(path -> !Files.isDirectory(path))
+                     .forEach(LambdaCheckedException.handlingConsumerWrapper(path -> {
+                         ZipEntry zipEntry = new ZipEntry(sourcePath.getParent().relativize(path).toString());
+                         zipOutputStream.putNextEntry(zipEntry);
+                         zipOutputStream.write(Files.readAllBytes(path));
+                         zipOutputStream.closeEntry();
+                     }, IOException.class));
+            }
+        }
 
-		for (Path root : fs.getRootDirectories()) {
-			Files.walkFileTree(root, finder);
-		}
+        if (deleteOriginal) {
+            for (Path dir : directories) {
+                Files.walk(dir, FileVisitOption.FOLLOW_LINKS)
+                     .sorted(Comparator.reverseOrder())
+                     .forEach(LambdaCheckedException.handlingConsumerWrapper(Files::deleteIfExists, IOException.class));
 
-		ArrayList<Path> files = finder.getFilePaths();
-		fs.close();
-		return files;
-	}
+            }
+        }
+    }
+
+
+    /**
+     * Finds a file with the specified name pattern in a zip file
+     *
+     * @param zipFile The zip file to search in
+     * @param pattern The pattern in the file inside the zip file
+     * @return List of located files
+     * @throws IOException When reading the zip file
+     */
+    public static List<Path> findFiles(Path zipFile, String pattern) throws IOException {
+
+        Path currDir = Paths.get("").toAbsolutePath();
+        zipFile = currDir.resolve(zipFile);
+
+        HashMap<Path, FileSystem> fileSystems = new HashMap<>();
+        FileSystem fs = fileSystems.get(zipFile);
+        if (fs == null) {
+            // throw new FileNotFoundException(jarFile.toString());
+            Map<String, String> env = new HashMap<>();
+            env.put("create", "false");
+            fs = FileSystems.newFileSystem(URI.create("jar:file:" + zipFile), env);
+            fileSystems.put(zipFile, fs);
+        }
+
+        Find finder = new Find(pattern, fs);
+
+        for (Path root : fs.getRootDirectories()) {
+            Files.walkFileTree(root, finder);
+        }
+
+        ArrayList<Path> files = finder.getFilePaths();
+        fs.close();
+        return files;
+    }
 }
