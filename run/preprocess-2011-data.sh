d787250dcc7d5dd3a82fe1a586fdfc5c98160971
wniroshan <s3467727@student.rmit.edu.au>
2019-05-30 09:35:45 +1000

    Requires sudo to install R libraries in run shell script. Plus fixed typo in 2011 preprocessing script

--- run/preprocess-2011-data.sh
+++ run/preprocess-2011-data.sh
@@ -7,5 +7,4 @@ CDIR=`pwd`
 cd $RSCRIPTDIR &&
 echo "Change dir:" $RSCRIPTDIR &&
 ./sa2preprocess.R --households ../data/melbourne-2011/raw/Households_2011_Greater_Melbourne_SA2.zip --persons ../data/melbourne-2011/raw/Persons_2011_Greater_Melbourne_SA2.zip --output ../data/melbourne-2011/generated/SA2/ --errorfile ../data/melbourne-2011/analysis/cleaning_error.csv &&
-cd $CDIR &&
-
+cd $CDIR
